5f391d87a0427c6bf765f25549a734598dd2a11d
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2019-11-26 17:03:59 +0200

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,19 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Animated } from "react-native";
 import { Icon } from "react-native-elements";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
-import Basker from './FontSComponent/Basker'
+import Basker from "./FontSComponent/Basker";
 
 export default function Header() {
-
-  
-
-
   return (
     <View style={styles.header}>
       <Icon name="menu" size={30} />
       {/* <Text style={styles.headerElement}></Text> */}
-<Basker text = 'Hello Basker' />
-      <Icon name="search" size={30} />
+      <Basker text="Hello Basker" />
+      <Icon name="search" size={30} /> 
     </View>
   );
 }
@@ -28,7 +24,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-around",
-    flexBasis: "14%",
+    flexBasis: "14%"
   },
   headerElement: {
     letterSpacing: 1,
